2151ca6b09ef052801bd46cfb36ed0485d33dee8
Natalia <natalia.roudneff@le-campus-numerique.fr>
2021-02-19 14:18:25 +0100

    passage en typescript

--- src/App.vue
+++ src/App.vue
@@ -1,89 +1,101 @@
-npm install -g @vue/cli<template>
+<template>
+<!--  <img alt="Vue logo" src="./assets/logo.png">-->
+<!--  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>-->
+
   <div id="app">
     <header id="app_header">
       <img src="./assets/logo-cn.png" alt="Logo Campus Numérique">
       <div>
-        <router-link to="/villes">Liste</router-link>
-        <router-link to="/carte">Carte</router-link>
+        <router-link to="/onenagros">Citations Kaamelott</router-link>
 
+        <router-link to="/villes">Liste Villes</router-link>
+
+        <router-link to="/carte">Carte</router-link>
       </div>
     </header>
-    <div id="map" />
     <router-view></router-view>
   </div>
 </template>
 
-<script>
-  // import mapboxgl from "mapbox-gl";
-  // import "mapbox-gl/dist/mapbox-gl.css";
-  // import { onMounted } from "vue";
-  // export default {
-  //   setup() {
-  //     onMounted(() => {
-  //       mapboxgl.accessToken =
-  //               "yourAccessToken";
-  //       const map = new mapboxgl.Map({
-  //         container: "map",
-  //         style: "mapbox://styles/mapbox/light-v9",
-  //       });
-  //       map.on('load', () => {
+<!--<script lang="ts">-->
+<!--import { Options, Vue } from 'vue-class-component';-->
+<!--import HelloWorld from './components/HelloWorld.vue';-->
+
+<!--@Options({-->
+<!--  components: {-->
+<!--    HelloWorld,-->
+<!--  },-->
+<!--})-->
+<!--export default class App extends Vue {}-->
+<!--</script>-->
+<script lang="ts">
+  import { defineComponent } from "vue";
+
+  export default defineComponent ({
+    name: 'App'
+  })
+  // import { Options, Vue } from 'vue-class-component';
+  // import HelloWorld from './components/HelloWorld.vue';
   //
-  //       });
-  //     });
-  //     return {};
+  // @Options({
+  //   components: {
+  //     HelloWorld,
   //   },
-
-export default {
-  name: 'App'
-
-
-}
+  // })
+  // export default class App extends Vue {}
 </script>
-
+<!--<style>-->
+<!--#app {-->
+<!--  font-family: Avenir, Helvetica, Arial, sans-serif;-->
+<!--  -webkit-font-smoothing: antialiased;-->
+<!--  -moz-osx-font-smoothing: grayscale;-->
+<!--  text-align: center;-->
+<!--  color: #2c3e50;-->
+<!--  margin-top: 60px;-->
+<!--}-->
+<!--</style>-->
 <style>
-html, body {
-  margin: 0;
-  padding: 0;
-}
+  html, body {
+    margin: 0;
+    padding: 0;
+  }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #262626;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #262626;
+  }
 
-#app_header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  background-color: #262626;
-  padding-right: 20px;
-}
+  #app_header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #262626;
+    padding-right: 20px;
+  }
 
-#app_header img {
-  height: 80px;
-  padding: 20px;
-}
+  #app_header img {
+    height: 80px;
+    padding: 20px;
+  }
 
-#app_header a {
-  color: white;
-  text-transform: uppercase;
-  padding: 20px;
-  text-decoration: none;
-}
+  #app_header a {
+    color: white;
+    text-transform: uppercase;
+    padding: 20px;
+    text-decoration: none;
+  }
 
-#map {
-  width: 100%;
-  height: 500px;
-}
+  #map {
+    width: 100%;
+    height: 500px;
+  }
 
-#map .marker {
-  width: 50px;
-  height: auto;
-  cursor: pointer;
-}
+  #map .marker {
+    width: 50px;
+    height: auto;
+    cursor: pointer;
+  }
 </style>
-
-
